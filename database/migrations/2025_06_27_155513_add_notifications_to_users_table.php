468054c685d358e2a5307e7e2cc5d5e586afb14d
Karol Krakowski <repus.soft@gmail.com>
2025-06-27 18:21:52 +0200

    fix

--- database/migrations/2025_06_27_155513_add_notifications_to_users_table.php
+++ database/migrations/2025_06_27_155513_add_notifications_to_users_table.php
@@ -24,31 +24,5 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['notifications', 'notification_settings']);
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
     }
 };
